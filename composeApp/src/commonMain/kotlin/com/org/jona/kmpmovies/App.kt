21d42130543269dd48d88d92501bea292b4f7d29
joNA <jitzian@gmail.com>
2024-08-17 12:15:51 -0400

    kmp movies js - Cleanup (Removing imports)

--- composeApp/src/commonMain/kotlin/com/org/jona/kmpmovies/App.kt
+++ composeApp/src/commonMain/kotlin/com/org/jona/kmpmovies/App.kt
@@ -1,20 +1,9 @@
 package com.org.jona.kmpmovies
 
-import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
-import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import org.jetbrains.compose.resources.painterResource
+import androidx.compose.runtime.Composable
 import org.jetbrains.compose.ui.tooling.preview.Preview
-
-import kmpmovies.composeapp.generated.resources.Res
-import kmpmovies.composeapp.generated.resources.compose_multiplatform
 
 @Composable
 @Preview
